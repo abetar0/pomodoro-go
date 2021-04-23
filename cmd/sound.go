ff95b37cfd2afff46f9577d394c3840f841a596e
Taro Abe <tarofuku827@gmail.com>
2021-04-24 01:30:05 +0900

    add some stuff

--- cmd/sound.go
+++ cmd/sound.go
@@ -1,14 +1,26 @@
-package main
+package cmd
 
 import (
     "log"
     "os/exec"
+    "github.com/spf13/cobra"
 )
 
-func main() {
+func Sound() {
     cmd := exec.Command("afplay", "/System/Library/Sounds/Blow.aiff")
     err := cmd.Run()
     if err != nil {
         log.Fatal(err)
     }
+}
+
+var rootCmd = &cobra.Command {
+        Use: "sound",
+        Run: func(cmd *cobra.Command, args []string) {
+                Sound()
+            },
+}
+
+func Execute() {
+    rootCmd.Execute()
 }
